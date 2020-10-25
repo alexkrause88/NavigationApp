2917ad271c69e30c583971e1ab923fbd56b27539
abc <abc@example.com>
2020-10-25 12:22:11 -0500

    Initial commit

---
+++ app/src/main/java/com/krause/navigationapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.krause.navigationapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bigButton:Button = findViewById(R.id.titlesub)
+
+        bigButton.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
