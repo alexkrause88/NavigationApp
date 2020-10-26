9df076e0f2550579e0ee6eaa700da86774a2e276
abc <abc@example.com>
2020-10-25 20:57:25 -0500

    initial start_final

--- app/src/main/java/com/krause/navigationapp/MainActivity.kt
+++ app/src/main/java/com/krause/navigationapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bigButton:Button = findViewById(R.id.main_button)
+        val cabage:TextView=findViewById(R.id.main_title_textview)
+        val roomNumberInput:EditText=findViewById(R.id.main_room_number_input)
+
 
         bigButton.setOnClickListener {
             val intent = Intent(this,SecondActivity::class.java)
             startActivity(intent)
         }
 
+        cabage.setOnClickListener{
+            val roomNumber = roomNumberInput.text.toString()
+            Toast.makeText(this@MainActivity,"your room # is $roomNumber",Toast.LENGTH_LONG).show()
+        }
     }
 }
