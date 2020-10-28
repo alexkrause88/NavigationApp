2e920b5426298efcebe66a04b7e45b06fe0bec26
abc <abc@example.com>
2020-10-27 21:42:06 -0500

    created a fourth page

--- app/src/main/java/com/krause/navigationapp/ThirdActivity.kt
+++ app/src/main/java/com/krause/navigationapp/ThirdActivity.kt
@@ -1,11 +1,21 @@
 package com.krause.navigationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ThirdActivity : AppCompatActivity() {
-    override fun  onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val bigButton: Button = findViewById(R.id.third_button)
+
+        bigButton.setOnClickListener {
+            val intent = Intent(this,FouthActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
