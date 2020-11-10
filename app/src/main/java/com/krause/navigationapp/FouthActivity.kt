fcb9cc30f19a5ac40815990420c6658cc4c6bda0
abc <abc@example.com>
2020-11-10 17:15:37 -0600

    added some logic to one button

--- app/src/main/java/com/krause/navigationapp/FouthActivity.kt
+++ app/src/main/java/com/krause/navigationapp/FouthActivity.kt
@@ -1,13 +1,31 @@
 package com.krause.navigationapp
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 
 class FouthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fouth)
+
+
+        val bigCheckBox:CheckBox = findViewById(R.id.checkbox_theater)
+
+        bigCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            Log.d("deanthonee","Event just happened")
+
+            if (isChecked){
+                //do something
+                Log.d("deanthonee"," our answer is $isChecked")
+                title = "Is On"
+            }
+            else{
+                Log.d("deanthonee"," xoxoxoxox our answer is $isChecked")
+                title = "Is Off"
+            }
+        }
     }
 }
