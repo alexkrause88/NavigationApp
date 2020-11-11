f98ebd285435401750425bb0006421d2554f50bc
abc <abc@example.com>
2020-11-10 22:41:08 -0600

    added more radiobutons and cleaned up code

--- app/src/main/java/com/krause/navigationapp/FouthActivity.kt
+++ app/src/main/java/com/krause/navigationapp/FouthActivity.kt
@@ -3,17 +3,29 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.CheckBox
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class FouthActivity : AppCompatActivity() {
+
+    lateinit var bigCheckBox: CheckBox
+    lateinit var bigSwitchButton: Switch
+    lateinit var radioGroup: RadioGroup
+    lateinit var radiobuttona:RadioButton
+    lateinit var b:RadioButton
+    lateinit var e:RadioButton
+    lateinit var d:RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fouth)
 
+        initViews()
+        initListener()
+    }
 
-        val bigCheckBox: CheckBox = findViewById(R.id.checkbox_theater)
-
+    private fun initListener() {
         bigCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
             Log.d("deanthonee", "Event just happened")
 
@@ -26,25 +38,6 @@ class FouthActivity : AppCompatActivity() {
                 title = "Is Off"
             }
         }
-
-        val bigRadioButton: RadioButton = findViewById(R.id.radio_theater)
-
-        bigRadioButton.setOnCheckedChangeListener { buttonView, isClicked ->
-            Log.d("Krausenburger", "Event just happened")
-
-            if (isClicked) {
-                //do something
-                Log.d("Krausenburger", " homie, did they click? $isClicked")
-                title = "Is On"
-            } else {
-                //do something else
-                Log.d("Krausenburger", " I think so: $isClicked")
-                title = "Is Off"
-
-            }
-        }
-
-        val bigSwitchButton: Switch = findViewById(R.id.switch_theater)
 
         bigSwitchButton.setOnCheckedChangeListener { buttonView, isSwitched ->
             Log.d("Krausenburger", "Event just happened")
@@ -61,5 +54,40 @@ class FouthActivity : AppCompatActivity() {
             }
         }
 
+        radioGroup.setOnCheckedChangeListener { _ , checkedId ->
+            Log.d("miko"," the item that was clicked id is $checkedId ")
+            Log.d("miko", " so the first id is ${radiobuttona.id}")
+            Log.d("miko", " so the second id is ${b.id}")
+            Log.d("miko", " so the third id is ${e.id}")
+            Log.d("miko", " so the fouth id is ${d.id}")
+
+            when(checkedId){
+                radiobuttona.id -> {
+                    Log.d("tero","Fisrt buuttoin was clicked homie")
+                }
+                b.id -> {
+                    Log.d("tero","second button was clicked and we need to do some magic here")
+                }
+                e.id -> {
+                    Log.d("tero","third button was clicked and we need to do some magic here")
+                }
+                d.id -> {
+                    Log.d("tero","fouth button was clicked and we need to do some magic here")
+                }
+                else ->{}
+            }
+
+        }
+
+    }
+
+    fun initViews(){
+        bigCheckBox = findViewById(R.id.checkbox_theater)
+        bigSwitchButton = findViewById(R.id.switch_theater)
+        radioGroup = findViewById(R.id.radio_group)
+        radiobuttona = findViewById(R.id.radio_theater)
+        b = findViewById(R.id.radio2_theater)
+        e = findViewById(R.id.radio3_theater)
+        d = findViewById(R.id.radio4_theater)
     }
 }
